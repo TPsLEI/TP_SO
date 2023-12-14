e32b750f0312911e555fe2467bc689c397ec2c50
Miguel Carvalho <carvalhomiguel286@gmail.com>
2023-12-14 18:34:51 +0000

    Inicio do Projeto

--- projeto/Kernel.java
+++ projeto/Kernel.java
@@ -2,4 +2,18 @@ package projeto;
 
 public class Kernel {
 
+    public void start() {
+        Middleware middleware = new Middleware();
+        middleware.getUserInput(this);
+    }
+
+    public void displayData(String data) {
+        System.out.println("Número: " + data);
+    }
+
+    public static void main(String[] args) {
+        Kernel kernel = new Kernel();
+
+        kernel.start();
+    }
 }
--- projeto/Middleware.java
+++ projeto/Middleware.java
@@ -1,5 +1,13 @@
 package projeto;
 
-public class Middleware {
+import java.util.Scanner;
 
+public class Middleware {
+    public void getUserInput(Kernel kernel) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insere um número: ");
+        String userInput = scanner.nextLine();
+
+        kernel.displayData(userInput);
+    }
 }
