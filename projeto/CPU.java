fb681df2ba586a5fc1a96a84eae5bc67a7999eca
José Diogo Teixeira de Magalhães <josedmagalhaes100809@gmail.com>
2023-12-20 23:45:21 +0000

    update wait for message

--- projeto/CPU.java
+++ projeto/CPU.java
@@ -1,5 +1,42 @@
 package projeto;
 
-public class CPU {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+import java.util.concurrent.LinkedBlockingQueue;
 
+public class CPU extends Thread {
+    private LinkedBlockingQueue<String> dataQueue;
+    private String csvFileName;
+
+    public CPU(LinkedBlockingQueue<String> dataQueue, String csvFileName) {
+        this.dataQueue = dataQueue;
+        this.csvFileName = csvFileName;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Insira uma mensagem:");
+                String message = scanner.nextLine();
+                if ("stop".equals(message)) {
+                    System.out.println("Programa encerrado.");
+                    break;
+                }
+
+                LocalDateTime timestamp = LocalDateTime.now();
+
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFileName, true))) {
+                    writer.write(timestamp + "," + message + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- projeto/Kernel.java
+++ projeto/Kernel.java
@@ -1,58 +1,12 @@
 package projeto;
 
-import java.util.Scanner;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Kernel {
-
-    static Scanner scanner = new Scanner(System.in);
-    static Middleware middleware = new Middleware();
-
-    public void receiveData(String name, String username, String password) {
-        System.out.println("Received data in Kernel:");
-        System.out.println("------------------------------");
-        System.out.println("Name     : " + name);
-        System.out.println("Username : " + username);
-        System.out.println("Password : " + password);
-        System.out.println("------------------------------");
-    }
-
-    public Estacao askForData() {
-        Estacao estacao = new Estacao();
-
-        System.out.println("Qual é o Nome? ");
-        String name = scanner.nextLine();
-        System.out.println("Qual é o Username? ");
-        String username = scanner.nextLine();
-        System.out.println("Qual é a Password? ");
-        String password = scanner.nextLine();
-
-        estacao.setName(name);
-        estacao.setUsername(username);
-        estacao.setPassword(password);
-
-        return estacao;
-    }
-
     public static void main(String[] args) {
-        Kernel kernel = new Kernel();
+        LinkedBlockingQueue<String> dataQueue = new LinkedBlockingQueue<>();
 
-        boolean dataProvided = false;
-        while (!dataProvided) {
-            Estacao estacao = kernel.askForData();
-
-            if (!estacao.name.isEmpty() && !estacao.username.isEmpty() && !estacao.password.isEmpty()) {
-                dataProvided = true;
-
-                Middleware middleware = new Middleware(estacao);
-                middleware.sendDataToKernel();
-            } else {
-                System.out.println("Por favor, preencha todos os campos.");
-            }
-        }
-
-        middleware.waitMessageFromStation();
-        middleware.sendMessageToStation("Olá");
-
-        scanner.close();
+        CPU thread = new CPU(dataQueue, "dados.csv");
+        thread.start();
     }
 }
