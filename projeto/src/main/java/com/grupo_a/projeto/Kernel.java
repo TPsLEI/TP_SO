a601821047f0ddaff14d86cbcce165cc455d7ee6
José Diogo Teixeira de Magalhães <josedmagalhaes100809@gmail.com>
2023-12-22 02:27:09 +0000

    Message after login

--- projeto/src/main/java/com/grupo_a/projeto/Kernel.java
+++ projeto/src/main/java/com/grupo_a/projeto/Kernel.java
@@ -5,15 +5,10 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class Kernel {
     public static void main(String[] args) {
-        LinkedBlockingQueue<String> dataQueue = new LinkedBlockingQueue<>();
 
         SwingUtilities.invokeLater(() -> {
-            // Middleware middleware = new Middleware(dataQueue);
             Login loginForm = new Login();
         });
 
-        MEM mem = new MEM("dados.csv");
-        CPU thread = new CPU(dataQueue, mem);
-        thread.start();
     }
 }
--- projeto/src/main/java/com/grupo_a/projeto/MenuPage.java
+++ projeto/src/main/java/com/grupo_a/projeto/MenuPage.java
@@ -2,10 +2,18 @@ package com.grupo_a.projeto;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class MenuPage extends BaseFrame {
+    LinkedBlockingQueue<String> dataQueue = new LinkedBlockingQueue<>();
+
     MenuPage() {
         setTitle("Menu");
+        Middleware middleware = new Middleware(dataQueue);
+
+        MEM mem = new MEM("dados.csv");
+        CPU thread = new CPU(dataQueue, mem);
+        thread.start();
     }
 
 }
