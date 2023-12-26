bde9d90ab0f4df8c34d99da1a6cf1020e2ced2fa
Carvalho286 <65737925+Carvalho286@users.noreply.github.com>
2023-12-26 01:30:41 +0000

    Update

--- projeto/src/main/java/com/grupo_a/projeto/GraphPage.java
+++ projeto/src/main/java/com/grupo_a/projeto/GraphPage.java
@@ -1,9 +1,18 @@
 package com.grupo_a.projeto;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GraphPage extends BaseFrame {
 
     public GraphPage() {
         super("Gráfico");
+        setSize(960, 600);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
 
     }
 }
+
