f10e312c071397923358a582ad0f0dc94b90402d
José Diogo Teixeira de Magalhães <josedmagalhaes100809@gmail.com>
2024-01-17 17:52:13 +0000

    Correções + Relatório

--- projeto/start.bat
+++
@@ -1,2 +0,0 @@
-javac -cp ./lib/miglayout.jar;./lib/miglayoutcore.jar;./lib/flatlaf.jar;./lib/json.jar;./lib/jfreechart.jar;./lib/jcommon.jar -d ./classes -encoding UTF-8 *.java
-java -cp .;./lib/miglayout.jar;./lib/miglayoutcore.jar;./lib/flatlaf.jar;./lib/json.jar;./lib/jfreechart.jar;./lib/jcommon.jar;./classes Kernel
